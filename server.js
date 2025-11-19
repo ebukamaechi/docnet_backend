3728fc59376c519c95c297dc7c893be75230206d
Chukwuebuka Amaechi <vamaefule9@gmail.com>
2025-11-19 23:27:16 +0100

    fourth commit

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const http = require("http");
+//const http = require("http");
 const swaggerUi = require("swagger-ui-express");
 const swaggerJsdoc = require("swagger-jsdoc");
 const connectDB = require("./config/db");
